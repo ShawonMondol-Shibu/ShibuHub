88a8dfdd169f9a5ded2eb630353266113d92f6ac
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-08-31 13:44:34 +0600

    header updated

--- components/layout/home/Header.tsx
+++ components/layout/home/Header.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/carousel";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-
+import Link from "next/link";
 
 type carouselType = {
   id: number;
@@ -19,6 +19,7 @@ type carouselType = {
   model: string;
   description: string;
   price: number;
+  discount: number;
 };
 
 type HeaderProps = {
@@ -30,32 +31,38 @@ export function Header({ headerData }: HeaderProps) {
     <Carousel className="w-full max-w-max">
       <CarouselContent className="bg-neutral-100">
         {headerData.slice(0, 5).map((item: carouselType) => {
-          const { id, image, title, price, model, description } = item;
+          const { id, image, title, price, model, description, discount } = item;
           return (
             <CarouselItem key={id}>
               <div>
                 <Card className="border-none shadow-none">
-                  <CardContent className="flex gap-10 items-center justify-center">
+                  <CardContent className="flex items-center justify-center gap-10">
                     <Image
                       src={image || "/images/image.png"}
                       width={500}
                       height={500}
                       alt={title}
-                      className="m-auto basis-1/3"
+                      className="basis-1/3"
                     />
-                    <hgroup className="space-y-4 w-xl m-auto basis-1/3">
-                      <CardTitle className="text-4xl">{title}</CardTitle>
-                      <p>{model}</p>
-                      <p className="line-clamp-2 text-lg">{description}</p>
-                      <span className="flex items-center">
+                    <hgroup className="space-y-5 w-xl basis-1/3">
+                      <CardTitle className="text-5xl text-indigo-950 font-bold leading-16 line-clamp-2">
+                        {title}
+                      </CardTitle>
+                      <p className="font-bold text-indigo-950">{model}</p>
+                      <p className="line-clamp-3 text-lg text-neutral-700">{description}</p>
+                      <p>
+
+                        <del className="text-xl font-bold"> Discount $ {discount}</del>
+                      </p>
+                      <span className="flex items-center gap-5">
                         <p className="text-xl font-bold">$ {price}</p>
 
                         <Button
                           variant={"outline"}
                           size={"lg"}
-                          className="size-12 px-20"
+                          className="size-12 px-20 hover:bg-indigo-500 hover:text-white hover:shadow-2xl"
                         >
-                          Get It now
+                          <Link href={`/${id}`}>Get It now</Link>
                         </Button>
                       </span>
                     </hgroup>
