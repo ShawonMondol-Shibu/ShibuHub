d82d5f5b0b8f32df56b3d70d0c7bd7100cf9a67a
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-09-09 00:14:00 +0600

    .

--- app/(website)/[id]/page.tsx
+++ app/(website)/[id]/page.tsx
@@ -1,14 +1,14 @@
 import ProductPage from "./ProductPage";
 
 // Static params generate for GitHub Pages (static export)
-export async function generateStaticParams() {
-  const res = await fetch("https://fakestoreapi.com/products");
-  const data = await res.json();
-
-  return data.map((product: { id: number }) => ({
-    id: product.id.toString(),
-  }));
-}
+// export async function generateStaticParams() {
+//   const res = await fetch("https://fakestoreapi.com/products");
+//   const data = await res.json();
+//   console.log(data);
+//   return data.map((product: { id: number }) => ({
+//     id: product.id.toString(),
+//   }));
+// }
 
 export default function Page({ params }: { params: { id: string } }) {
   return <ProductPage id={params.id} />;
